265767f03db4016a1e8106721a750a7ebc0dc31a
Michael Tu <tuzhucheng@outlook.com>
2014-11-25 02:18:27 -0800

    dummy data for get_classes API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import make_response
-import json
+import urllib, urllib2
 app = Flask(__name__)
 app.config.from_envvar('CONFIG_FILE')
 
@@ -11,7 +11,18 @@ def index():
 
 @app.route('/get_classes', methods=['GET'])
 def get_classes():
-    return make_response(res)
+    # TODO: replace hardcoded, static data with query in GET request
+    level = 'under'
+    sess = 1151
+    subject = 'CS'
+    cournum = 341
+    url = 'http://www.adm.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl'
+    values = {'level': level, 'sess': sess, 'subject': subject, 'cournum': cournum}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    res = urllib2.urlopen(req)
+    page = res.read()
+    return make_response(page)
 
 
 if __name__ == '__main__':
