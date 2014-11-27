363ac2b5ca894a82a561f64598d97e1445f5f9b0
Michael Tu <tuzhucheng@outlook.com>
2014-11-27 01:30:41 -0800

    quotation mark bug fix

--- clients/cron/call_uw_course_alerter.py
+++ clients/cron/call_uw_course_alerter.py
@@ -10,7 +10,7 @@ def main():
                 'email': 'youremail@example.com'}]
     for course in courses:
         encoded_query = urllib.urlencode(course)
-        subprocess.call(['curl', '-X', 'POST', '-H', '"Cache-Control: no-cache"', '-H', '"Content-Type: application/x-www-form-urlencoded"', '-d', encoded_query, api_url])
+        subprocess.call(['curl', '-X', 'POST', '-H', 'Cache-Control: no-cache', '-H', 'Content-Type: application/x-www-form-urlencoded', '-d', encoded_query, api_url])
 
 if __name__ == '__main__':
     main()
