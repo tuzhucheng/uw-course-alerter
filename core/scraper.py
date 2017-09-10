dfa92db8ba32a2209dacab04d9b14279f5f37f3d
Michael Tu <tuzhucheng@outlook.com>
2017-09-10 18:19:43 -0400

    Support seminars in addition to lectures and labs

--- core/scraper.py
+++ core/scraper.py
@@ -9,15 +9,17 @@ def extract_blocks(page):
         table_cells = row.find_all('td')
         if table_cells:
             component_and_section = table_cells[1].get_text().rstrip()
-            if 'LEC' in component_and_section or 'LAB' in component_and_section:
-                component, section = component_and_section.split(' ')
-                block = {'component': component,
-                         'section': section,
-                         'enroll_cap': int(table_cells[6].get_text().rstrip()),
-                         'enroll_total': int(table_cells[7].get_text().rstrip()),
-                         'time': table_cells[10].get_text().rstrip(),
-                         'room': table_cells[11].get_text().rstrip(),
-                         'prof': table_cells[12].get_text().rstrip() if len(table_cells) > 12 else ''}
-                blocks.append(block)
+            for ctype in ['LEC', 'LAB', 'SEM']:
+                if ctype in component_and_section:
+                    component, section = component_and_section.split(' ')
+                    block = {'component': component,
+                             'section': section,
+                             'enroll_cap': int(table_cells[6].get_text().rstrip()),
+                             'enroll_total': int(table_cells[7].get_text().rstrip()),
+                             'time': table_cells[10].get_text().rstrip(),
+                             'room': table_cells[11].get_text().rstrip(),
+                             'prof': table_cells[12].get_text().rstrip() if len(table_cells) > 12 else ''}
+                    blocks.append(block)
+                    break
     return blocks
 
